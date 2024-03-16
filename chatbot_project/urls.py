d9d4107aef3cf1ef928ed10e45f63fb3cf88dbf1
Soham Nale <97284087+SohamNale@users.noreply.github.com>
2024-03-16 13:26:23 +0530

    Update urls.py

--- chatbot_project/urls.py
+++ chatbot_project/urls.py
@@ -17,11 +17,9 @@ Including another URLconf
 # chatbot_project/urls.py
 from django.contrib import admin
 from django.urls import include, path
-from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('chatbot/', include('chatbot_app.urls')),
-    path('', views.home, name='home'),
 ]
 
